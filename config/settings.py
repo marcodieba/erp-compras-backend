9e07331c62fc1f3f1f73e8a60381dcf2b385f6d5
marcodieba <marcodieba@gmail.com>
2026-06-02 13:22:02 -0300

    Adiciona Whitenoise para servir ficheiros estáticos em produção

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Adicione isto para permitir o cache e compressão dos ficheiros
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # --- UPLOADS & ANEXOS ---
 MEDIA_URL = '/media/'
