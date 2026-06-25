31efd579dceaef627c4ecc5ee92de95b1dcbf28d
marcodieba <marcodieba@gmail.com>
2026-06-25 15:31:17 -0300

    CORRECAO PERMISSOES JW

--- config/asgi.py
+++ config/asgi.py
@@ -3,6 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from apps.notificacoes.routing import websocket_urlpatterns
+from apps.notificacoes.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp_compras.settings')
 
